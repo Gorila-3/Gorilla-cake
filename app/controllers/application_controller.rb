ac18d58874a1e39cdc82b78866a3afbd53ff8dc3
“kaikai8812” <sauna.kaikai@gmail.com>
2021-02-17 00:29:50 +0000

    2/16作成内容（かい）

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+#↓　会員ログインの際、追加したカラムを許可する記述 
+ before_action :customers_configure_permitted_parameters , if: :devise_controller?
+
+ def customers_configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number])
+ end
+#　会員ログインの際、追加したカラムを許可する記述 
+  
 end
--- app/controllers/customers/items_controller.rb
+++ app/controllers/customers/items_controller.rb
@@ -1,8 +1,14 @@
 class Customers::ItemsController < ApplicationController
+  
   def index
+   @items = Item.all
   end
 
   def show
+    @item = Item.find(params[:id])
+    @tax = 0.1 #消費税１０%を、ここで定義しています。
+    @tax_in_price = @item.price * (1 + @tax)
+    @tax_in_price = @tax_in_price.to_i #上とあわした二行で、値段を三点区切りで表示しています。なんか汚いのでいい方法あったら
   end
 
   def top
