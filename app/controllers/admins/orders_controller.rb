a70c6dbdfd67b7804c1eeac3a3891a9720077a92
“kaikai8812” <sauna.kaikai@gmail.com>
2021-02-20 01:18:32 +0000

    管理者側ー注文履歴ー未完成

--- app/controllers/admins/orders_controller.rb
+++ app/controllers/admins/orders_controller.rb
@@ -1,10 +1,30 @@
 class Admins::OrdersController < ApplicationController
+  
   def index
+    @orders = Order.all
+    # @total_amount = detail_total_amount(order_details)
+    # binding.pry
   end
 
   def show
+    @order = Order.find(params[:id])
+    @order_details = @order.order_details   #中間テーブル自体を変数にしておくことで、親テーブルどちらの情報も取ってこれるようにしている。
+    # binding.pry
   end
 
   def update
+    # binding.pry
+    @order = Order.find(params[:id])
+    @order.update(status: order_update_params[:status].to_i)
+    # binding.pry
+    redirect_to admins_order_path
   end
+  
+  private
+  
+  def order_update_params
+    params.require(:order).permit(:status)
+  end
+
+  
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,7 @@
 class Item < ApplicationRecord
  has_many :cart_items, dependent: :destroy
- belongs_to :category, dependent: :destroy
+ has_many :order_details
+ has_many :orders, through: :order_details
+ belongs_to :category
  attachment :image
 end
